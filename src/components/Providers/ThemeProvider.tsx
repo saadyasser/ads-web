fd82b084e4f490663da9981dc649d29a5a6dc53b
mohammed kullab <moh.kull200@gmail.com>
2024-05-22 23:21:40 +0300

    configured dark mode theme

---
+++ src/components/Providers/ThemeProvider.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { ThemeProvider, useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+
+type Props = {
+  children: string | React.JSX.Element | React.JSX.Element[] | React.ReactNode;
+};
+
+export const DarkModeProvider = ({ children }: Props) => {
+  const [mounted, setMounted] = useState<boolean>(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default DarkModeProvider;
