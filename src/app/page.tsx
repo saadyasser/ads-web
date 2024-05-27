38254e59cace52cda7252c917d355810f9b1fa1a
mohammed kullab <moh.kull200@gmail.com>
2024-05-27 15:08:34 +0300

    edits on app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
 "use client";
-
-import DropDown from "@/components/Dropdown";
-import ThemeSwitcher from "@/components/ThemeSwitcher";
-
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <p>
-        <code className="text-red-100 dark:text-gray-700">
-          testing dark mode{" "}
-        </code>
-      </p>
+    <main className="flex flex-col items-center min-h-screen p-24">
       <p className="text-2xl font-georgia">font georgia !!!</p>
       <br />
       <p className="text-2xl font-bold font-georgia">font georgia bold!!!</p>
@@ -18,8 +9,6 @@ export default function Home() {
       <p className="text-2xl font-inter">font inter !!!</p>
       <br />
       <p className="text-2xl font-bold font-inter">font inter bold!!!</p>
-      <DropDown />
-      <ThemeSwitcher />
     </main>
   );
 }
