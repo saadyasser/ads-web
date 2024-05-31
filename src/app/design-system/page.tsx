654f81ddf01a21080d5042ab06dc6f60c8a1cc89
mohammed kullab <moh.kull200@gmail.com>
2024-05-31 22:49:51 +0300

    fixed page error

--- src/app/design-system/page.tsx
+++ src/app/design-system/page.tsx
@@ -2,7 +2,7 @@
 
 import Button from "@/components/Button";
 import { SunIcon } from "@/lib/@react-icons";
-export const DesignSystem = () => {
+const DesignSystem = () => {
   return (
     <main className="flex flex-col items-center min-h-screen p-12 md:p-24">
       <p className="text-2xl font-georgia">font georgia !!!</p>
