b952699a19cf6c3a3a092116ef3adfe99d4b9116
mohammed kullab <moh.kull200@gmail.com>
2024-06-02 13:51:09 +0300

    created footer

---
+++ src/data/footer_data.ts
@@ -0,0 +1,22 @@
+export const FOOTER_NAVIGATION = {
+  quickLinks: [
+    { name: "Home", href: "/" },
+    { name: "UI Components", href: "/ui-components" },
+    { name: "Web & Mobile", href: "/web-mobile" },
+    { name: "Web Templates", href: "/web-templates" },
+    { name: "About us", href: "/about-us" },
+  ],
+  help: [
+    { name: "Contact us", href: "#" },
+    { name: "Our Policies", href: "#" },
+    { name: "Register", href: "#" },
+    { name: "Login", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+  ],
+  socialMedia: [
+    { name: "LinkedIn", href: "#" },
+    { name: "Instagram", href: "#" },
+    { name: "Twitter", href: "#" },
+    { name: "Facebook", href: "#" },
+  ],
+} as const;
