75a662e5d1d681157f01e67b4f54fb5f196e341d
mohammed kullab <moh.kull200@gmail.com>
2024-09-06 02:00:17 +0300

    added vercel speed insights

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -4,6 +4,7 @@ import { childrenType } from "@/types";
 import DarkModeProvider from "./DarkModeProvider";
 import { ToastContainer } from "react-toastify";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "react-toastify/dist/ReactToastify.css";
 
 type Props = {
@@ -16,6 +17,7 @@ export const Providers = ({ children }: Props) => {
       {children}
       <ToastContainer />
       <Analytics />
+      <SpeedInsights />
     </DarkModeProvider>
   );
 };
