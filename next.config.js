57b8116b48ab3732745f7cd4697b857528120ba1
saadyasser <saad.wadi.229@gmail.com>
2024-10-19 10:43:06 +0300

    add analizer tool

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (process.env.ANALYZE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'server',
+          openAnalyzer: true,
+        })
+      );
+    }
+    return config;
+  },
   images: {
     remotePatterns: [
       {
