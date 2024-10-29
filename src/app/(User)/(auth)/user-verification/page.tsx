1cbaccb33f2a8e2232b9747e0a58e76eb8340e8b
saadyasser <saad.wadi.229@gmail.com>
2024-10-29 19:48:14 +0200

    adding user-verificationuser page

---
+++ src/app/(User)/(auth)/user-verification/page.tsx
@@ -0,0 +1,8 @@
+import VerificationCodeSent from "@/features/UserManagement/components/VerificationFormSent";
+import React from "react";
+
+const UserVerification = () => {
+  return <VerificationCodeSent />;
+};
+
+export default UserVerification;
